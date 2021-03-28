91a3fa576517d26b8860398eeb878054722a8906
sahil-777 <turbothunderboltz@gmail.com>
2021-03-29 00:59:39 +0530

    Minor changes, loggedin Person state Maintaining is yet to be done

--- src/JS/user.js
+++ src/JS/user.js
@@ -1,5 +1,7 @@
 const auth = firebase.auth();
 const db = firebase.database();
+const rootRef=db.ref();
+let MainAdmin='techmoAdmin';
 
 function logout(){
     firebase.auth().signOut().then(() => {
@@ -13,12 +15,35 @@ function logout(){
     });
 }
 
-firebase.auth().onAuthStateChanged(function(user) {
+/*
+auth.onAuthStateChanged(function(user) {
     if (user) {
-      console.log(auth.currentUser.email);
+        console.log(user.email);
+        rootRef.child(MainAdmin).once("value")
+        .then(function(snapshot) {
+            let isStoreOwner = snapshot.child(user.uid).exists(); // true/false
+            console.log(isStoreOwner);
+            if(isStoreOwner==false)
+            window.location="notAllowed.html";
+        })
+        .catch((error) => {
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            document.getElementById('error-msg').innerHTML=errorMessage;
+            console.log(errorCode, errorMessage); 
+        });
     } 
     else {
       window.location="login.html";
     }
 });
- 
+*/
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        console.log(user.email);
+    } else {
+        window.location="login.html";
+    }
+});
