5e29ef9c8b346d47205ff127c1c98a4c6ad49e4a
sahil-777 <turbothunderboltz@gmail.com>
2021-03-28 16:29:48 +0530

    Added: Redirecting to Admin/user page based on credentials

--- src/JS/login.js
+++ src/JS/login.js
@@ -1,18 +1,33 @@
 const auth = firebase.auth();
 const db = firebase.database();
-
+const rootRef=db.ref();
+let MainAdmin='techmoAdmin';
 function login(){ 
     let email=document.getElementById('person-email').value;
     let password=document.getElementById('person-password').value;
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
         var user = userCredential.user;
-        console.log(user);
-        window.location="index.html"; //After loggedIn successfully
+        console.log(user.uid);
+        rootRef.child(MainAdmin).once("value")
+            .then(function(snapshot) {
+                let isStoreOwner = snapshot.child(user.uid).exists(); // true/false
+                //True: If uid exists in admin level i.e. person is admin/storeowner
+                if(isStoreOwner==true)
+                    window.location="admin.html";
+                else
+                    window.location="user.html";
+            })
+            .catch((error) => {
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                document.getElementById('error-msg').innerHTML=errorMessage;
+                console.log(errorCode, errorMessage); 
+            })
     })
     .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        let errorCode = error.code;
+        let errorMessage = error.message;
         document.getElementById('error-msg').innerHTML=errorMessage;
         console.log(errorCode, errorMessage);
     });
