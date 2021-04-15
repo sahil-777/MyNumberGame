28f9c5dcfac3cfbe6079bd20cee408ebb90bd7e6
sahil-777 <turbothunderboltz@gmail.com>
2021-04-15 23:54:51 +0530

    Firebase db config

---
+++ Teqmo-Admin/assets/firebase/firebase-config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBFwJkHQHb4IuU0Xuloba3d9FSZayB1eqw",
+    authDomain: "numberpredictor-prd.firebaseapp.com",
+    databaseURL: "https://numberpredictor-prd-default-rtdb.firebaseio.com",
+    projectId: "numberpredictor-prd",
+    storageBucket: "numberpredictor-prd.appspot.com",
+    messagingSenderId: "333354525993",
+    appId: "1:333354525993:web:b8b713f2e609d9c545c338"
+};
+firebase.initializeApp(firebaseConfig);
